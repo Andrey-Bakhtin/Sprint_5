b23f8b6967a6ca0bd8e724b2d8d89a8203ee0310
Andrey-Bakhtin <elnutal@yandex.ru>
2026-06-07 03:07:31 +0500

    Create test_registration.py

---
+++ tests/test_registration.py
@@ -0,0 +1,53 @@
+from locators import Locators
+from urls import BASE_URL
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+class TestRegistration:
+
+    def test_successful_registration(self, driver, user_data):
+        """Успешная регистрация пользователя"""
+        driver.get(BASE_URL)
+
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(Locators.PERSONAL_ACCOUNT_BUTTON)
+        ).click()
+
+        driver.find_element(*Locators.REGISTER_LINK).click()
+
+        driver.find_element(*Locators.NAME_INPUT).send_keys(user_data["name"])
+        driver.find_element(*Locators.EMAIL_INPUT).send_keys(user_data["email"])
+        driver.find_element(*Locators.PASSWORD_INPUT).send_keys(user_data["password"])
+
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(Locators.LOGIN_HEADER)
+        )
+
+        assert driver.find_element(*Locators.LOGIN_HEADER).is_displayed()
+
+    def test_registration_with_invalid_password(self, driver, user_data):
+        """Регистрация с невалидным паролем (менее 6 символов)"""
+        invalid_user_data = user_data.copy()
+        invalid_user_data["password"] = "12345"
+
+        driver.get(BASE_URL)
+
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(Locators.PERSONAL_ACCOUNT_BUTTON)
+        ).click()
+
+        driver.find_element(*Locators.REGISTER_LINK).click()
+
+        driver.find_element(*Locators.NAME_INPUT).send_keys(invalid_user_data["name"])
+        driver.find_element(*Locators.EMAIL_INPUT).send_keys(invalid_user_data["email"])
+        driver.find_element(*Locators.PASSWORD_INPUT).send_keys(invalid_user_data["password"])
+
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+
+        error_message = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(Locators.PASSWORD_ERROR)
+        )
+
+        assert error_message.is_displayed()
