583a531f8978854a2098f18a57cbf65aad1c4b63
Andrey-Bakhtin <elnutal@yandex.ru>
2026-06-07 03:01:27 +0500

    Create helps.py

---
+++ helps.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+
+def generate_email():
+    email = f'Andrey_Bakhtin_47{random.randint(100,999)}@yandex.ru'
+    return email
+def generate_password():
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    return password
