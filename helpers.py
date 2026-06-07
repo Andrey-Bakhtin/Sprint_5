c6368973dd9113f2e265a83e9e7a65c1a361f1bd
Andrey-Bakhtin <elnutal@yandex.ru>
2026-06-07 17:06:30 +0500

    Update and rename helps.py to helpers.py

--- helpers.py
+++ helpers.py
@@ -5,6 +5,13 @@ import string
 def generate_email():
     email = f'Andrey_Bakhtin_47{random.randint(100,999)}@yandex.ru'
     return email
+
 def generate_password():
     password = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
     return password
+
+def get_user_data():
+    email = generate_email()
+    password = generate_password()
+    name = 'Андрей'
+    return {'email': email, 'password': password, 'name': name}
